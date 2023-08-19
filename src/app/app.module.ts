48c0ca9d68d472314b7d37627939d7ba171d2037
diego-silva02 <diegoadsbr010@gmail.com>
2023-08-19 12:36:08 -0300

    feat: created table with mock data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './views/home/home.component';
 import { ParticipantsCrudComponent } from './views/participants-crud/participants-crud.component';
+import { ParticipantsReadComponent } from './components/participants/participants-read/participants-read.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -14,7 +15,10 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import { ParticipantsReadComponent } from './components/participants/participants-read/participants-read.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTableModule } from '@angular/material/table';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
 
 
 @NgModule({
@@ -35,6 +39,11 @@ import { ParticipantsReadComponent } from './components/participants/participant
     MatListModule,
     MatCardModule,
     MatButtonModule,
+    MatFormFieldModule,
+    MatTableModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
