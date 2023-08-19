b1c6487e91556ee33c769b88ff3de323909af8db
diego-silva02 <diegoadsbr010@gmail.com>
2023-08-19 12:19:20 -0300

    feat: created participant model

---
+++ src/app/shared/models/participant.model.ts
@@ -0,0 +1,14 @@
+export interface Participant {
+    id?: number,
+    name: string,
+    foneNumber?: string,
+    instagram?: string,
+    parentsContact?: string,
+    age?: number,
+    cpf?: string,
+    rg?: string,
+    wasPaid: boolean,
+    valuePaid?: number,
+    foodRestriction?: string,
+    otherThings?: string
+}
