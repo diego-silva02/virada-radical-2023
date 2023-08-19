9380fdc0828ba866f90180c4f2010682a9dfad6b
diego-silva02 <diegoadsbr010@gmail.com>
2023-08-19 12:53:23 -0300

    feat: created table with correct object mock

--- src/app/components/participants/participants-read/participants-read.component.ts
+++ src/app/components/participants/participants-read/participants-read.component.ts
@@ -1,25 +1,6 @@
 import { Component } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
+import { Participant } from 'src/app/shared/models/participant.model';
 
 @Component({
   selector: 'app-participants-read',
@@ -30,12 +11,40 @@ export class ParticipantsReadComponent {
 
   constructor() {}
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  participantsList: Participant[] = [
+    {
+      id: 1,
+      name: "Robert Costa",
+      foneNumber: "085940028922",
+      instagram: "@johndoe",
+      parentsContact: "9876543210",
+      age: 20,
+      cpf: "123.456.789-01",
+      rg: "12345678",
+      wasPaid: true,
+      valuePaid: 100,
+      foodRestriction: "Vegetariano, Alergia a nozes",
+      otherThings: ""
+    }, {
+      id: 2,
+      name: "Jane Barros",
+      foneNumber: "085989224002",
+      instagram: "@janesmith",
+      parentsContact: "5551234567",
+      age: 16,
+      cpf: "987.654.321-09",
+      rg: "87654321",
+      wasPaid: false,
+      foodRestriction: "Nenhuma",
+      otherThings: ""
+    },
+  ];
+
+  displayedColumns: string[] = ['name','age', 'foneNumber', 'wasPaid'];
+  dataSource = new MatTableDataSource(this.participantsList);
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
-
 }
