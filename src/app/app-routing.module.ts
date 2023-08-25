4ed8bea2afa1fca5b1d0977b084d6422be79918e
diego-silva02 <diegoadsbr010@gmail.com>
2023-08-25 18:10:25 -0300

    fix: fixed participants-read route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
 import { ParticipantsCrudComponent } from './views/participants-crud/participants-crud.component';
+import { ParticipantsReadComponent } from './components/participants/participants-read/participants-read.component';
 
 const routes: Routes = [{
     path: "",
     component: HomeComponent
   },
-  {
-    path: "participantes",
-    component: ParticipantsCrudComponent
-  },
+  { path: "participantes", component: ParticipantsCrudComponent, children: [
+    { path: "tabela", component: ParticipantsReadComponent }
+  ]}
+  // { path: "gerenciar-pessoa", component: GerenciarPessoasComponent, canActivate: [AuthGuard], children: [
+  //   { path: "criar", component: CriarPessoaComponent }
+  // ]},
 ];
 
 @NgModule({
