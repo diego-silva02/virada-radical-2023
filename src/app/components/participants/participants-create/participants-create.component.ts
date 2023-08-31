74d17283c4dd9a1e5a175b10d5c4b8e2e0cb64b1
diego-silva02 <diegoadsbr010@gmail.com>
2023-08-31 16:05:07 -0300

    feat: started to create participants-create fields

--- src/app/components/participants/participants-create/participants-create.component.ts
+++ src/app/components/participants/participants-create/participants-create.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-participants-create',
@@ -7,4 +9,32 @@ import { Component } from '@angular/core';
 })
 export class ParticipantsCreateComponent {
 
+  formGroup!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router
+    ) {
+    this.formGroup = this.formBuilder.group({
+      name: ['', Validators.required],
+      phoneNumber: [''],
+      instagram: [''],
+      parentsContact: [''],
+      age: [null],
+      cpf: [''],
+      rg: [''],
+      wasPaid: [false, Validators.required],
+      valuePaid: [0],
+      foodRestriction: [''],
+      otherThings: ['']
+    })
+  }
+  
+  createParticipant(): void {
+    console.log(this.formGroup.value);
+  }
+
+  back(): void {
+    this.router.navigate(['/participants/tabela'])
+  }
 }
